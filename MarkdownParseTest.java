17852e507f0e9bc1095e12b9d038caa78fcd3be2
akar247 <akar@ucsd.edu>
2022-04-21 16:54:10 -0700

    Add Valid tests to MarkdownParse Test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,31 @@
 import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Files;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testTestFile() throws IOException {
+        ArrayList<String> endLinks = null;
+        try{
+            endLinks = MarkdownParse.getLinks(Files.readString(Path.of("test-file.md")));
+        }
+        catch(IOException io){
+            System.out.println(io.getMessage());
+            throw io;
+        }
+        
+        System.out.println(endLinks);
+        assertEquals(List.of("https://something.com", "some-thing.html"), endLinks);
     }
 }
